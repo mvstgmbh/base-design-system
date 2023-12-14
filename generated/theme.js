46a72559e264083f467c5ab30eeb33c719fe2cca
Bernat Pericàs <bernatpericasserra97@gmail.com>
2023-12-14 11:38:39 +0100

    Update tailwind theme

---
+++ generated/theme.js
@@ -0,0 +1,137 @@
+export default {
+  "theme": {
+    "colors": {
+      "primary": {
+        "100": "#ebf0ff",
+        "500": "#668ef7",
+        "700": "#3368f3",
+        "900": "#255ef3"
+      },
+      "error": {
+        "100": "#ffd8d8",
+        "500": "#ef4444",
+        "900": "#7f1d1d"
+      },
+      "success": {
+        "100": "#caffee",
+        "500": "#65c366",
+        "900": "#064e3b"
+      },
+      "warning": {
+        "100": "#fff6d0",
+        "500": "#f59e0b",
+        "900": "#78350f"
+      },
+      "neutral-background": {
+        "100": "#f3f3f3",
+        "200": "#cfcfcf",
+        "300": "#9a9a9a",
+        "400": "#686868",
+        "500": "#545454",
+        "600": "#393939",
+        "700": "#2e2e2e",
+        "800": "#1d1d1d",
+        "900": "#0d0d0d",
+        "000": "#ffffff"
+      }
+    },
+    "spacing": {
+      "zero": "0px",
+      "xs": "4px",
+      "sm": "8px",
+      "md": "12px",
+      "lg": "16px",
+      "xl": "20px",
+      "2xl": "24px",
+      "3xl": "32px",
+      "4xl": "48px"
+    },
+    "borderRadius": {
+      "zero": "0px",
+      "xs": "4px",
+      "sm": "8px",
+      "md": "12px",
+      "lg": "16px",
+      "xl": "24px",
+      "max": "999px"
+    },
+    "fontSize": {
+      "xs": [
+        "12px",
+        {
+          "lineHeight": "16px",
+          "letterSpacing": "0px"
+        }
+      ],
+      "sm": [
+        "14px",
+        {
+          "lineHeight": "20px",
+          "letterSpacing": "0px"
+        }
+      ],
+      "base": [
+        "16px",
+        {
+          "lineHeight": "24px",
+          "letterSpacing": "0px"
+        }
+      ],
+      "lg": [
+        "18px",
+        {
+          "lineHeight": "28px",
+          "letterSpacing": "0px"
+        }
+      ],
+      "xl": [
+        "20px",
+        {
+          "lineHeight": "28px",
+          "letterSpacing": "0px"
+        }
+      ],
+      "2xl": [
+        "24px",
+        {
+          "lineHeight": "32px",
+          "letterSpacing": "0px"
+        }
+      ],
+      "3xl": [
+        "30px",
+        {
+          "lineHeight": "36px",
+          "letterSpacing": "0px"
+        }
+      ],
+      "4xl": [
+        "36px",
+        {
+          "lineHeight": "40px",
+          "letterSpacing": "0px"
+        }
+      ],
+      "5xl": [
+        "48px",
+        {
+          "lineHeight": "48px",
+          "letterSpacing": "0px"
+        }
+      ],
+      "6xl": [
+        "60px",
+        {
+          "lineHeight": "60px",
+          "letterSpacing": "0px"
+        }
+      ]
+    },
+    "screens": {
+      "xl": "1536px",
+      "lg": "1280px",
+      "md": "991px",
+      "sm": "478px"
+    }
+  }
+}
