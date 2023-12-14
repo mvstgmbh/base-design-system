c18b10716cbe231e55b792442a32abd8482902f2
Breno <breno@Brenos-MBP.fritz.box>
2023-12-14 16:29:43 +0100

    WIP

--- src/components/atoms/Button.tsx
+++ src/components/atoms/Button.tsx
@@ -1,28 +1,31 @@
-import { VariantProps, cva } from 'class-variance-authority';
-import React, { ButtonHTMLAttributes, FC, forwardRef } from 'react';
-import { twMerge } from 'tailwind-merge';
+import { VariantProps, cva } from "class-variance-authority";
+import React, { ButtonHTMLAttributes, FC, forwardRef } from "react";
+import { twMerge } from "tailwind-merge";
 
-const buttonVariants = cva(`px-md py-sm rounded-sm text-white`, {
+const buttonVariants = cva(`px-md py-sm rounded-md text-white`, {
   variants: {
     variant: {
-      primary: 'bg-primary-500 hover:bg-primary-900',
-      warning: 'bg-warning-500 hover:bg-warning-900',
-      error: 'bg-error-500 hover:bg-error-900',
-      success: 'bg-success-500 hover:bg-success-900',
+      primary: "bg-primary-500 hover:bg-primary-900",
+      secondary:
+        "border-neutral-background-900 text-neutral-background-900/90 border hover:text-light-900 hover:bg-neutral-background-900",
+      warning: "bg-warning-500 hover:bg-warning-900",
+      error: "bg-error-500 hover:bg-error-900",
+      success: "bg-success-500 hover:bg-success-900",
     },
     size: {
-      small: 'py-xs px-sm',
-      large: 'text-xl py-3 px-6',
+      small: "py-sm px-lg",
+      large: "text-xl py-3 px-6",
     },
   },
   defaultVariants: {
-    size: 'small',
-    variant: 'primary',
+    size: "small",
+    variant: "primary",
   },
 });
 
 interface ButtonVariants extends VariantProps<typeof buttonVariants> {}
-const buttonStyles = (variants: ButtonVariants) => twMerge(buttonVariants(variants));
+const buttonStyles = (variants: ButtonVariants) =>
+  twMerge(buttonVariants(variants));
 
 interface ButtonProps extends VariantProps<typeof buttonVariants> {
   children: React.ReactNode;
@@ -34,8 +37,17 @@ interface ButtonProps
   ref?: React.Ref<HTMLButtonElement>;
 }
 
-export const Button: FC<ButtonProps> = forwardRef(({ size, variant, ...props }, ref) => {
-  return <button ref={ref} type='button' className={buttonStyles({ variant, size })} {...props} />;
-});
+export const Button: FC<ButtonProps> = forwardRef(
+  ({ size, variant, ...props }, ref) => {
+    return (
+      <button
+        ref={ref}
+        type="button"
+        className={buttonStyles({ variant, size })}
+        {...props}
+      />
+    );
+  },
+);
 
-Button.displayName = 'Button';
+Button.displayName = "Button";
--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,13 +1,46 @@
-import { HeaderSection } from '../demo/HeaderSection';
-import { Hero } from '../demo/Hero';
-import { Navbar } from '../demo/Navbar';
+import { PropsWithChildren } from "react";
+import { Typography } from "../atoms/Typography";
+import { Navbar } from "../demo/Navbar";
+
+export const Badge = ({
+  variant,
+  children,
+}: { variant: string } & PropsWithChildren) => (
+  <div className="">
+    <Typography variant="default" as="span">
+      {children}
+    </Typography>
+  </div>
+);
 
 export const HomePage = () => {
   return (
-    <div className='bg-white'>
-      <Navbar />
-      <Hero />
-      <HeaderSection />
+    <div className="bg-white flex flex-col gap-[120px]">
+      <div className="flex flex-col gap-6xl">
+        <Navbar />
+        <div className="grid grid-cols-2 gap-4xl">
+          <div className="w-full max-w-2xl flex flex-col gap-3xl">
+            <div className="w-full"> rocket</div>
+            <div className="w-full flex gap-lg">
+              <Badge variant="primary">Whats new</Badge>
+              <Badge variant="secondary">Lorem ipsum color</Badge>
+            </div>
+            <Typography as="h1" variant="primary">
+              Deploy to the cloud with confidence
+            </Typography>
+            <Typography as="p" variant="secondary">
+              Lorem ipsum dolor sit amet consectetur. Ac sem viverra proin ac.
+              Velit pretium quisque enim molestie. Nullam sagittis consequat
+              malesuada nisl amet amet lectus.
+            </Typography>
+          </div>
+          <img
+            className="border-neutral-900/10 border-[12px] rounded-md"
+            src="https://s3-alpha-sig.figma.com/img/6701/d4b8/26e15621aeecbf718d003ba9f67beb9c?Expires=1703462400&Signature=X2FEnebZYuHzf7Td5zjq2SQMuRP1mGIl~nHm1ISEY-FW4OeXzwLkn5c8mWsBcyX3htk6tkCxq43iF4MutkTiV2ouAgWwuvg-LmKRO7NXlwuyeuT0tObIOpWoPxBY4gmaW775ZnTkapQk5oJw9titWBoh~FSWufErmfdLNem7Xf3JgW2-32sGK9zeu22k-SJZKWxmH68q3zIwotjIFBIDnOBs8YIIfKa1LHRtbLE2XnSe5NaOYhzsAwsjSPMfAq6wIBC3puRtN~KkvNyaQdTWOkAl1Kk-mpcUU-OsKGOvuAGb390P4JAMq2YpOMcO6yle938i95qTnNsBFVkT2D~KaQ__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
+            alt="hero_image"
+          />
+        </div>
+      </div>
     </div>
   );
 };
